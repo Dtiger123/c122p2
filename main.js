98ad9c78080972986ed8bcc33903818f43b6eeab
Dtiger123 <88206528+Dtiger123@users.noreply.github.com>
2022-03-28 21:04:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+x = 0;
+y = 0;
+draw_circle = "";
+draw_rect = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("status").innerHTML = "System is listening please speak"; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var content = event.results[0][0].transcript;
+
+   document.getElementById("status").innerHTML = "The Speech has been recognized as: " + content;}
